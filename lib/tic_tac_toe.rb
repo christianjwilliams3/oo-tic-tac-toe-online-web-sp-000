91f10356c0ee93c7d4d88c7747505496f892703d
christian williams <christian.williamsj@gmail.com>
2019-09-17 21:11:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,5 +49,13 @@ def turn_count
   return turn
 end
 
-
+def current_player
+  num_turns = turn_count
+  if num_turns % 2 == 0
+    player = "X"
+  else
+    player = "O"
+  end
+  return player
+end
 end

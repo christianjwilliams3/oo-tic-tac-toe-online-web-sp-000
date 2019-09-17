1ad9a022c5705414862c35a1d95adcb4b5a16078
christian williams <christian.williamsj@gmail.com>
2019-09-17 19:53:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,11 +35,5 @@ def position_taken?(location)
   @board[location] != " " && @board[location] != ""
 end
 
-def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken?(board, index)
-    true 
-  else 
-    false
-  end
-end
+
 end
